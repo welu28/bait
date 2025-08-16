2589a0ff6710bc4e052422e572b2d9f62f516291
welu28 <27luw@millburn.org>
2025-08-15 23:18:37 -0400

    Polyml

---
+++ polyml-5.9.2/libpolyml/savestate.h
@@ -0,0 +1,29 @@
+/*
+    Title:  savestate.h - Save and Load state
+
+    Copyright (c) 2007, 2015, 2019 David C.J. Matthews
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License version 2.1 as published by the Free Software Foundation.
+    
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+    
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+#ifndef SAVESTATE_H_INCLUDED
+#define SAVESTATE_H_INCLUDED
+
+PolyObject *InitHeaderFromExport(struct _exportDescription *exports);
+
+extern struct _entrypts savestateEPT[];
+
+#endif
+
